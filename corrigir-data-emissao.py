ad92716bcca232d98fecf121a1df02848dd723c9
Pablo <nordiws@gmail.com>
2022-02-01 14:24:49 -0300

    fix: alterando data emissao para xml Neo Fols

--- corrigir-data-emissao.py
+++ corrigir-data-emissao.py
@@ -17,15 +17,12 @@ def fixXML(xmlFile):
         root = tree.getroot()
 
         for child in root:
-            for itens in child:
-                if itens.tag == "C":
-                    for value in itens:
-                        if value.tag == "DataEmissao":
-                            dtnow = datetime.now()
-                            day_string = dtnow.strftime("%Y-%m-%d")
-                            hour_string = dtnow.strftime("%H:%M:%S")
-                            value.text = day_string + "T" + hour_string
-                            time.sleep(1)
+            if child.tag == "dtEmis":
+                dtnow = datetime.now()
+                day_string = dtnow.strftime("%Y-%m-%d")
+                hour_string = dtnow.strftime("%H:%M:%S")
+                child.text = day_string + "T" + hour_string
+                time.sleep(1)
 
             output_file = xmlFile.replace(".xml", "") + "_CORRIGIDA.xml"
 
@@ -33,7 +30,8 @@ def fixXML(xmlFile):
                 tree.write(f, encoding='unicode')
             global counter
             counter += 1
-            logger.error("{}. Arquivo {} corrigido com sucesso.".format(counter, output_file))
+            logger.error("{}. Arquivo {} corrigido com sucesso.".format(
+                counter, output_file))
 
 
 files = []
