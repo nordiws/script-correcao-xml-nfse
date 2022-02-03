5a395382363f68ba2d95c112b7f06d90308ae5e2
Pablo <nordiws@gmail.com>
2022-02-03 11:46:13 -0300

    feat: script para alterar emissao e valores

---
+++ corrigir-data-emissao-issRetido-rps-neofols.py
@@ -0,0 +1,65 @@
+import xml.etree.ElementTree as ET
+import logging
+from os import walk
+
+logger = logging.getLogger()
+logger.propagate = True
+counter = 0
+serviceValue = ""
+
+
+def fixXML(xmlFile):
+
+    with open("./erradas/" + xmlFile, encoding='utf-8') as file:
+
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        for rps in root:  # root = <NeoGrid>
+            for child in rps:
+                if(child.tag == "dtEmis"):
+                    child.text = "2022-01-30T00:00:00"
+
+                if (child.tag == "servico"):
+                    for value in child:
+                        if (value.tag == "vlServicos"):
+                            global serviceValue
+                            serviceValue = value.text
+
+                        if(value.tag == "vlDeducoes"):
+                            if(value.text != "0.00"):
+                                value.text = "0.00"
+
+                        if(value.tag == "issRet"):
+                            value.text = "2"
+
+                        if(value.tag == "vlISSRet"):
+                            value.text = "0.000"
+
+                        if(value.tag == "baseCalculo"):
+                            value.text = serviceValue
+
+                        if(value.tag == "vliquiNFSe"):
+                            value.text = serviceValue
+
+            output_file = xmlFile.replace(".xml", "") + "_CORRIGIDA.xml"
+
+            with open("./corrigidas/" + output_file, 'w') as f:
+                tree.write(f, encoding='unicode')
+            global counter
+            counter += 1
+            logger.error("{}. Arquivo {} corrigido com sucesso.".format(
+                counter, output_file))
+
+
+files = []
+path = './erradas'
+
+for (dirpath, dirnames, filenames) in walk(path):
+    files.extend(filenames)
+    break
+
+for rps in files:
+    fixXML(rps)
+
+wait = input("\nDigite 'Enter' para finalizar o script.")
